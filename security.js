f0b9d8eaa5653830365a790c9ae27c6cbd424fd1
Javad <168000154+hello23445@users.noreply.github.com>
2025-09-11 17:44:22 +0400

    Add files via upload

--- security.js
+++ security.js
@@ -1,11 +1,8 @@
 export const blockedTokens = ['awsuoDiAt6U4LR8j', 'lgtrdfhreedghtesghtss', ''];
 export const texpereriv = '' //Уже, Скоро, ...
 
-const badWords = ['говно', 'хейт', 'порно', 'Порно']
+const badWords = ['говно', 'хейт', 'порно', 'Порно', '*', '"', "'"]
 if (badWords.includes(localStorage.getItem('userName'))){
     alert(localStorage.getItem('lang') === 'ru' ? 'Заданное имя нарушает наши правила.\nПожалуйста задайте другое имя.' : 'The name you entered violates our rules.\nPlease enter a different name.')
     window.location.href = 'user_profile.html'
 }
-
-
-
