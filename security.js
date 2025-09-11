db0f0d32d094ae6adbe069da746fe6a9296d8814
Javad <168000154+hello23445@users.noreply.github.com>
2025-09-11 16:51:52 +0400

    Update security.js

--- security.js
+++ security.js
@@ -1,4 +1,4 @@
-export const blockedTokens = ['awsuoDiAt6U4LR8j', 'lgtrdfhreedghtesghtss', ''];
+export const blockedTokens = ['awsuoDiAt6U4LR8j', 'lgtrdfhreedghtesghtss', 'GUo80GZ1KU7Jm9G4'];
 export const texpereriv = '' //Уже, Скоро, ...
 
 const badWords = ['говно', 'хейт', 'порно', 'Порно']
@@ -6,4 +6,5 @@ if (badWords.includes(localStorage.getItem('userName'))){
     alert(localStorage.getItem('lang') === 'ru' ? 'Заданное имя нарушает наши правила.\nПожалуйста задайте другое имя.' : 'The name you entered violates our rules.\nPlease enter a different name.')
     window.location.href = 'user_profile.html'
 }
+
 
