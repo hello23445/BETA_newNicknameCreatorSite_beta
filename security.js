c4cdcd994f9916cd5fe90cb128e6a6ca95b7ce7c
Javad <168000154+hello23445@users.noreply.github.com>
2025-09-20 23:59:09 +0400

    Add files via upload

--- security.js
+++ security.js
@@ -1,7 +1,8 @@
 export const blockedTokens = ['awsuoDiAt6U4LR8j', 'lgtrdfhreedghtesghtss', ''];
 export const texpereriv = '' //Уже, Скоро, ...
+localStorage.setItem('m', texpereriv);
 
-const badWords = ['говно', 'хейт', 'порно', 'Порно', '*', '"', "'", 'аноним', 'Аноним', 'admin', 'Админ', 'админ', 'Admin', 'moder', 'Moder', 'модер', 'Модер', 'модератор', 'Модератор', 'модерат', 'Продавец премиума', 'продавец премиума', 'Продавец Премиума', 'seller of premium', 'Seller of premium', 'Seller Of Premium', 'seller Of Premium', 'seller of Premium', 'seller of premium account', 'Seller of premium account', 'Seller Of Premium Account', 'seller Of Premium Account', 'seller of Premium Account', 'seller of premium Account'];
+const badWords = ['говно', 'хейт', 'порно', 'Порно', '*', '"', "'", 'аноним', 'Аноним', 'admin', 'Админ', 'админ', 'Admin', 'moder', 'Moder', 'модер', 'Модер', 'модератор', 'Модератор', 'модерат', 'Продавец премиума', 'продавец премиума', 'Продавец Премиума', 'seller of premium', 'Seller of premium', 'Seller Of Premium', 'seller Of Premium', 'seller of Premium', 'seller of premium account', 'Seller of premium account', 'Seller Of Premium Account', 'seller Of Premium Account', 'seller of Premium Account', 'seller of premium Account', '👑Премиум', '👑Premium'];
 if (badWords.includes(localStorage.getItem('userName'))){
     alert(localStorage.getItem('lang') === 'ru' ? 'Заданное имя нарушает наши правила.\nПожалуйста задайте другое имя.' : 'The name you entered violates our rules.\nPlease enter a different name.')
     window.location.href = 'user_profile.html'
